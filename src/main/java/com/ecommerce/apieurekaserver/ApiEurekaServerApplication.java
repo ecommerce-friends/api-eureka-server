5aaeff5bd6fee65a8bbfaa12693c81ea4b02510f
Raphael Freitas <raphael.freitas@netshoes.com>
2018-05-25 12:11:16 -0300

    create project

---
+++ src/main/java/com/ecommerce/apieurekaserver/ApiEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.apieurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEurekaServerApplication.class, args);
+	}
+}
